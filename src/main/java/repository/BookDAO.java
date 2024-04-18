b222c2286a124bdfa80dc7be4cb3b5e757789310
bitcocom <bitcocom@empas.com>
2024-04-18 12:59:57 +0900

    2024-4-18

--- src/main/java/repository/BookDAO.java
+++ src/main/java/repository/BookDAO.java
@@ -1,5 +1,7 @@
 package repository;
-// JDBC -> MyBatis-> Spring Mybatis-> | Hibernate(ORM) -> JPA(?)
+// JDBC -> MyBatis(mybatis.org)-> Spring Mybatis-> | Hibernate(ORM) -> JPA(?)
+// JDBC -> Java 소스코드 + SQL :  유지보수어렵다, 생산성이 떨어진다(?).
 public class BookDAO {
+
 
 }
